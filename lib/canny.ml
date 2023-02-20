12544a5ad2a1ef940ab602a6dfa8f7fc8b4f710f
Oscar Lahaie <oscarlahaie83@gmail.com>
2023-02-20 15:50:25 +0100

    canny update

--- lib/canny.ml
+++ lib/canny.ml
@@ -38,7 +38,7 @@ let noise_reduction_gray ~image ~mask_size =
   let sum, convolution_array = gaussian_mask mask_size in
   let ty = Image.ty image in
   let heigth, width, _ = Image.shape image in
-  let rgb_image = Image.v ty rgb heigth width in
+  let resimage = Image.v ty gray heigth width in
   let apply_convolution x y =
     Image.set_pixel new_image x y (Pixel.v gray [ 0. ]);
     for i = 0 to mask_size - 1 do
@@ -65,11 +65,10 @@ let noise_reduction_gray ~image ~mask_size =
     let current_pixel = Image.get_pixel new_image x y in
     let current_data = Pixel.get current_pixel 0 in
 
-    Image.set_pixel rgb_image x y
-      (Pixel.v rgb [ current_data; current_data; current_data ])
+    Image.set_pixel resimage x y (Pixel.v gray [ current_data ])
   in
   Image.for_each_pixel (fun x y _ -> apply_convolution x y) image;
-  rgb_image
+  resimage
 
 let noise_reduction ~image ~mask_size =
   let new_image = Image.copy image in
@@ -144,6 +143,42 @@ let gradient_map ?(gradient_floor = 0.) (image : (_, _, Color.hsv) Image.t) =
   Image.for_each_pixel (fun x y _ -> gradient_evalutation x y) image;
   Image.convert ty rgb gradient_image
 
+(** Prend en entrée une image au format Gray et renvoie une carte de ses gradients en fonction de la value *)
+let gradient_map_gray ?(gradient_floor = 0.)
+    (image : (_, _, Color.gray) Image.t) =
+  let ty = Image.ty image in
+  let heigth, width, _ = Image.shape image in
+  let gradient_image = Image.v ty rgb heigth width in
+  let gradient_x x y =
+    let left_value =
+      try Pixel.get (Image.get_pixel image (x - 1) y) 0 with _ -> 0.
+    in
+    let right_value =
+      try Pixel.get (Image.get_pixel image (x + 1) y) 0 with _ -> 0.
+    in
+    right_value -. left_value
+  in
+  let gradient_y x y =
+    let up_value =
+      try Pixel.get (Image.get_pixel image x (y + 1)) 0 with _ -> 0.
+    in
+    let down_value =
+      try Pixel.get (Image.get_pixel image x (y - 1)) 0 with _ -> 0.
+    in
+    up_value -. down_value
+  in
+  let gradient_evalutation x y =
+    let grad_x = gradient_x x y in
+    let grad_y = gradient_y x y in
+    let gradient_tot = sqrt ((grad_x *. grad_x) +. (grad_y *. grad_y)) in
+    Image.set_pixel gradient_image x y
+      (Pixel.v Color.rgb
+         (List.init 3 (fun _ ->
+              if gradient_tot >= gradient_floor then gradient_tot else 0.)))
+  in
+  Image.for_each_pixel (fun x y _ -> gradient_evalutation x y) image;
+  Image.convert ty rgb gradient_image
+
 let gradient_map_rgb ?(gradient_floor = 0.) (image : (_, _, Color.rgb) Image.t)
     =
   let ty = Image.ty image in
@@ -159,6 +194,7 @@ let gradient_map_rgb ?(gradient_floor = 0.) (image : (_, _, Color.rgb) Image.t)
              *. Pixel.get (Image.get_pixel image (x - 1) y) 1
           +. Pixel.get (Image.get_pixel image (x - 1) y) 0
              *. Pixel.get (Image.get_pixel image (x - 1) y) 0)
+        /. 3.
       with _ -> 0.
     in
     let right_value =
@@ -170,6 +206,7 @@ let gradient_map_rgb ?(gradient_floor = 0.) (image : (_, _, Color.rgb) Image.t)
              *. Pixel.get (Image.get_pixel image (x + 1) y) 1
           +. Pixel.get (Image.get_pixel image (x + 1) y) 0
              *. Pixel.get (Image.get_pixel image (x + 1) y) 0)
+        /. 3.
       with _ -> 0.
     in
     right_value -. left_value
@@ -184,6 +221,7 @@ let gradient_map_rgb ?(gradient_floor = 0.) (image : (_, _, Color.rgb) Image.t)
              *. Pixel.get (Image.get_pixel image x (y + 1)) 1
           +. Pixel.get (Image.get_pixel image x (y + 1)) 0
              *. Pixel.get (Image.get_pixel image x (y + 1)) 0)
+        /. 3.
       with _ -> 0.
     in
     let down_value =
@@ -195,6 +233,7 @@ let gradient_map_rgb ?(gradient_floor = 0.) (image : (_, _, Color.rgb) Image.t)
              *. Pixel.get (Image.get_pixel image x (y - 1)) 1
           +. Pixel.get (Image.get_pixel image x (y - 1)) 0
              *. Pixel.get (Image.get_pixel image x (y - 1)) 0)
+        /. 3.
       with _ -> 0.
     in
     up_value -. down_value
